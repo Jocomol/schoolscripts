6f991125d85da968cdeaff36676f9ae97b6f7512
Jocomol <joelmeier08@gmail.com>
2020-10-01 15:35:33 +0200

    - added .idea to .gitignore - finished the grades.py

--- grades.py
+++ grades.py
@@ -1,16 +1,33 @@
 #!/usr/bin/env python3
 import argparse
 import json
+import colorful
+
+gradefile_path = "/home/joco/.grades/grades.json"  # Path of the gradefile
+courses = ["Deutsch", "Mathematik", "Physik", "Geschichte und Politik", "Franzoesisch", "Chemie", "Wirtschaft"]
+courses_id = ["german", "math", "phyisk", "history", "french", "chemistry", "economics"]  # ID's of the different courses
+
+# This Method
+def print_courses():
+    number = 0
+    for course_name_string in courses:
+        print(str(number) + ": " + course_name_string)
+        number += 1
+
+def colored_grade(grade):
+    if (grade < 4):
+        return colorful.red(grade)
+    elif (grade < 4.5):
+        return colorful.yellow(grade)
+    else:
+        return colorful.green(grade)
 
 
-# TODO Flake8 and Documentation
-gradefile_path = "/home/joco/.grades/grades.json"
-
-# TODO short flags
 parser = argparse.ArgumentParser(description='Script to add grades and to calculate grades')
-parser.add_argument('--add', help='adds grade', type=float, default=0.0, action="store")
-parser.add_argument('--course', help='specifies course', type=str, default="", action="store")  # TODO better input here
-parser.add_argument('--list', help='lists all grades', action='store_true')
+parser.add_argument('-a', '--add', help='adds grade', type=float, default=0.0, action="store")
+parser.add_argument('-c', '--course', help='specifies course', type=int, default=0, action="store")
+parser.add_argument("-l", '--list', help='lists all grades', action='store_true')
+parser.add_argument("-C", "--courses", help="list all courses", action='store_true')
 
 args = parser.parse_args()
 
@@ -19,41 +36,44 @@ with open(gradefile_path) as json_file:
 
 if args.add != 0.0:
     grade = args.add
-    course_name = ""
-    if args.course != "":
-        course_name = args.course
+    course_id = 0
+    if args.course != 0:
+        course_id = args.course
     else:
-        # TODO better input here
-        course_name = input("Please enter the course you want to add the grade too: ")
+        print_courses()
+        course_id = int(input("Please enter the course id you want to add the grade too: "))
 
     try:
-        data[course_name]["grades"].append(grade)
-        print("Adding " + str(grade) + " to course " + course_name)
+        data[courses_id[course_id]]["grades"].append(grade)
+        print("Adding " + str(grade) + " to course " + courses[course_id])
     except KeyError:
-        print("Class: " + course_name + " not found")
-        # TODO print all courses
+        print("Course ID: " + courses[course_id] + " not found")
+        print_courses()
         parser.print_help()
         exit(1)
 
     with open(gradefile_path, 'w') as outfile:
         json.dump(data, outfile)
 
+elif args.courses:
+    print_courses()
+
 elif args.list:
-    # TODO Beautiful output
     mean_sum = 0
     number_off_courses = 0
     for course in data.items():
         number_off_courses += 1
-        print(course[1]["name"])
+        print(colorful.bold(course[1]["name"]))
         grade_sum = 0
         number_of_grades = 0
         for grade in course[1]["grades"]:
             grade_sum += grade
             number_of_grades += 1
-            print("-" + str(grade))
+            print("-" + colored_grade(grade))
         mean = grade_sum / number_of_grades
-        print("Durchschnitt: " + str(mean) + "\n")
+
+        print("mean: " + colored_grade(mean) + "\n")
         mean_sum += mean
-    print("Gesamtdurchschnitt " + str((mean_sum / number_off_courses).__round__(2)))
+    print("total mean " + colored_grade((mean_sum / number_off_courses).__round__(2)))
 else:
     parser.print_help()
