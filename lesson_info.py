b5e369c27a58716e60ae6e3577e2bc5242a5bfc4
Jocomol <joelmeier08@gmail.com>
2020-10-13 11:47:55 +0200

    - streamlined lesson_info.py - you can now see what lessons are coming next

--- lesson_info.py
+++ lesson_info.py
@@ -10,8 +10,6 @@ with open(lessonsfile_path) as json_file:
     data = json.load(json_file)
     weekday = datetime.today().weekday()
     now = datetime.now()
-    currentLesson = None
-    nextLesson = None
     timeTillBreak = None
     foundLesson = False
     if weekday >= 5:
@@ -20,29 +18,20 @@ with open(lessonsfile_path) as json_file:
     for lesson in data[weekday]:
         starttime = now.replace(hour=lesson["starttimehour"], minute=lesson["starttimeminute"], second=0, microsecond=0)
         endtime = now.replace(hour=lesson["endtimehour"], minute=lesson["endtimeminute"], second=0, microsecond=0)
-        if foundLesson:
-            nextLesson = lesson
-            break
+        if starttime > now < endtime:
+            print(colorful.bold(". ") + lesson["name"] + ", " + lesson["room"] + ", " + str(lesson["starttimehour"]) + ":" + str(lesson["starttimeminute"]) + "-" + str(lesson["endtimehour"]) + ":" + str(lesson["endtimeminute"]))
         elif starttime < now < endtime:
-            currentLesson = lesson
-            timeTillBreak = endtime - now
-            foundLesson = True
-    if currentLesson is None:
-        print(colorful.red("No Lesson running at the moment"))
-        exit(0)
+            print("---")
+            print(colorful.bold_green("Weekday: ") + days[weekday])
+            print(colorful.bold_green("Current time: ") + str(now.strftime("%H:%M")))
+            print("---")
+            print(colorful.bold_green("Lesson: ") + lesson["name"])
+            print(colorful.bold_green("Teacher: ") + lesson["teacher"])
+            print(colorful.bold_green("Room: ") + lesson["room"])
+            print(colorful.bold_green("Starttime: ") + str(lesson["starttimehour"]) + ":" + str(lesson["starttimeminute"]))
+            print(colorful.bold_green("Endtime: ") + str(lesson["endtimehour"]) + ":" + str(lesson["endtimeminute"]))
+            print(colorful.bold_green("Time till break: ") + str(timeTillBreak))
+            print("---")
+            print("Upcoming Lessons:")
 
-    print("---")
-    print(colorful.bold_green("Weekday: ") + days[weekday])
-    print(colorful.bold_green("Current time: ") + str(now.strftime("%H:%M")))
-    print("---")
-    print(colorful.bold_green("Lesson: ") + currentLesson["name"])
-    print(colorful.bold_green("Teacher: ") + currentLesson["teacher"])
-    print(colorful.bold_green("Room: ") + currentLesson["room"])
-    print(colorful.bold_green("Starttime: ") + str(currentLesson["starttimehour"]) + ":" + str(currentLesson["starttimeminute"]))
-    print(colorful.bold_green("Endtime: ") + str(currentLesson["endtimehour"]) + ":" + str(currentLesson["endtimeminute"]))
-    print(colorful.bold_green("Time till break: ") + str(timeTillBreak))
-    print("---")
-    if nextLesson is not None:
-        print(colorful.bold("Next Lesson: ") + nextLesson["name"] + ", " + nextLesson["room"] + ", " + str(nextLesson["starttimehour"]) + ":" + str(nextLesson["starttimeminute"]) + "-" + str(nextLesson["endtimehour"]) + ":" + str(nextLesson["endtimeminute"]))
-        print("---")
 
