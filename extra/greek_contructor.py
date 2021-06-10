04323d22f45a37fc454f9b130abfe540efaa013a
Otto Piramuthu <obp2@illinois.edu>
2021-06-10 07:06:36 -0400

    remove need for `re`

--- extra/greek_contructor.py
+++ extra/greek_contructor.py
@@ -1,7 +1,5 @@
-import re
 filename = ("./greek_letters")
 with open(filename) as file:
     for line in file:
-        line = re.sub("\s+", ",", line.strip())
-        line = line.split(",")
-        print("print(\"" + line[0] + " " + line[1] + " " + line[2].strip() + "\")")
+        line = ' '.join(line.strip().split())
+        print(f'print("{line}")')
